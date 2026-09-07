c16e1af6226c171001e03c6d5e7c5f312291e846
Md Thowsif <mdthowsif2211@gmail.com>
2026-09-07 15:01:55 +0530

    new up

--- server.js
+++ server.js
@@ -50,6 +50,10 @@ app.use((req, res) => {
 
 // Error handler
 app.use((err, req, res, next) => {
+  if (err instanceof SyntaxError && err.status === 400 && 'body' in err) {
+    return res.status(400).json({ message: 'Request body must be valid JSON' });
+  }
+
   console.error(err.stack);
   res.status(500).json({ message: 'Something went wrong!', error: err.message });
 });
